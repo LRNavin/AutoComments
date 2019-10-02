5c780422176e56f389fb0de3d27d8b2425818625
NavinLR <lr.navin@yahoo.com>
2019-10-02 17:51:45 +0200

    Update java_files_creator.py

--- preproc/java_files_creator.py
+++ preproc/java_files_creator.py
@@ -10,6 +10,8 @@ raw_data_folder = "data/"
 base_folder = "java_code_"
 sub_folder  = "data"
 
+# Get AST - Only First 100 code - boolean
+get_ast_full_file = False
 
 def save_code_in_javafile(to_write_path, code):
     f = open(to_write_path + "/Input.java", "w")
@@ -23,7 +25,7 @@ for file in raw_data_files:
     file_path = raw_data_folder + file + ".json"
     with open(file_path, 'r') as f:
         for index, line in enumerate(f):
-            if index == 100:
+            if not get_ast_full_file and index == 100:
                 break
             print(f"Writing Java Snippet No:{index}")
             to_write_path = curr_base_folder + '/' + sub_folder + str(index)
